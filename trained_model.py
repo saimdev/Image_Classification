d3215e6831a36241f495528c4aa05a6c606645c6
saimdev <saimabbasi486@gmail.com>
2023-07-04 23:01:51 +0500

    Changes

--- trained_model.py
+++ trained_model.py
@@ -3,22 +3,12 @@ from tensorflow.keras import datasets, layers, models
 import cv2 as cv
 import matplotlib.pyplot as plt
 
-# (training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
-
-# training_images, testing_images = training_images/255, testing_images/255
-
 class_names= ["Plane", "Car", "Bird", "Cat", "Deer", "Dog", "Frog", "Horse", "Ship", "Truck"]
 
-# training_images = training_images[:20000]
-# training_labels = training_labels[:20000]
-# testing_images = testing_images[:5000]
-# testing_labels = testing_labels[:5000]
-
 model = models.load_model('image_classifier.model')
-img = cv.imread('horse.jpg')
+img = cv.imread('deer.jpg')
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
-
-plt.imshow(img)
 prediction = model.predict(np.array([img])/255)
 index = np.argmax(prediction)
 
